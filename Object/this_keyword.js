25e6a243a70aee89d1d2cd2cf140b11445e4c4bc
shourovahmd <moh.abuhanifa@gmail.com>
2022-08-05 21:47:40 +0600

    implicit binding

--- Object/this_keyword.js
+++ Object/this_keyword.js
@@ -8,5 +8,28 @@ let country = {
     return this.name;
   },
 };
+console.log(country.countryName());
 
-console.log(country.countryName())
+//implicit binding
+
+var printPersonName = function (obj) {
+  obj.printName = function () {
+    console.log(this.name);
+  };
+};
+
+var Rahim = {
+  name: "Rahim",
+  age: 25,
+};
+
+var Karim = {
+  name: "Karim",
+  age: 22,
+};
+
+printPersonName(Rahim);
+printPersonName(Karim);
+
+Karim.printName();
+Rahim.printName();
